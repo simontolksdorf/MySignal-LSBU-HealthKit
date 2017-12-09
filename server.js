6af0d042ce723ffdf67b0848dbcdb99a5efb2ace
simontolksdorf <simontolksdorf@gmx.de>
2017-12-09 16:12:34 +0000

    fix

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
-console.log("App listening on port " + port + "(Port 80 with running container)");
+console.log("App listening on port " + port);
